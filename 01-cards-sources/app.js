707e02a14ae4dfd376b19076467f62ca0b87e125
kex <yakexa@gmail.com>
2022-01-26 17:51:17 +0300

    add cards functionality

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,15 @@
+const slides =document.querySelectorAll('.slide')
+for(const slide of slides){
+    
+    slide.addEventListener('click',()=>{
+        clearActiveFunctions()
+        
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveFunctions(){
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
